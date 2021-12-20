886b90705d0ce9f0500aad902cc1b49cde9f2112
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-12-20 19:26:36 +0100

    Fix not passing test

--- src/test/java/com/erapulus/server/web/controller/UniversityControllerTest.java
+++ src/test/java/com/erapulus/server/web/controller/UniversityControllerTest.java
@@ -28,6 +28,8 @@ class UniversityControllerTest {
     private static final int UNIVERSITY_ID_2 = 2;
     private static final String UNIVERSITY_NAME_1 = "university1";
     private static final String UNIVERSITY_NAME_2 = "university2";
+    private static final String LOGO_URL_1 = "logoUrl1";
+    private static final String LOGO_URL_2 = "logoUrl2";
 
     @Autowired
     ApplicationContext applicationContext;
@@ -51,17 +53,19 @@ class UniversityControllerTest {
                   "payload": [
                     {
                       "id": 1,
-                      "name": "university1"
+                      "name": "university1",
+                      "logoUrl": "logoUrl1"
                     },
                     {
                       "id": 2,
-                      "name": "university2"
+                      "name": "university2",
+                      "logoUrl": "logoUrl2"
                     }
                   ],
                   "message": null
                 }""";
-        var university1 = UniversityListDto.builder().name(UNIVERSITY_NAME_1).id(UNIVERSITY_ID_1).build();
-        var university2 = UniversityListDto.builder().name(UNIVERSITY_NAME_2).id(UNIVERSITY_ID_2).build();
+        var university1 = UniversityListDto.builder().name(UNIVERSITY_NAME_1).id(UNIVERSITY_ID_1).logoUrl(LOGO_URL_1).build();
+        var university2 = UniversityListDto.builder().name(UNIVERSITY_NAME_2).id(UNIVERSITY_ID_2).logoUrl(LOGO_URL_2).build();
         when(universityService.listEntities()).thenReturn(Mono.just(List.of(university1, university2)));
 
         // when-then
