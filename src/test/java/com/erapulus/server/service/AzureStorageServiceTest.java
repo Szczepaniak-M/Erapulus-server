800471ef13e831bb79f3c2bbd91e35123bb3d3eb
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-12-26 22:43:23 +0100

    Add test for delete file from Storage

--- src/test/java/com/erapulus/server/service/AzureStorageServiceTest.java
+++ src/test/java/com/erapulus/server/service/AzureStorageServiceTest.java
@@ -4,19 +4,18 @@ import com.azure.storage.blob.BlobAsyncClient;
 import com.azure.storage.blob.BlobContainerAsyncClient;
 import com.azure.storage.blob.models.BlockBlobItem;
 import com.erapulus.server.configuration.AzureStorageProperties;
+import com.erapulus.server.database.model.DocumentEntity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.buffer.*;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.http.codec.multipart.SynchronossPartHttpMessageReader;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import java.nio.ByteBuffer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,7 +49,7 @@ class AzureStorageServiceTest {
         String path = "app/path";
         String resultPath = "https://example.blob.core.windows.net/example/app/path";
         when(blobContainerAsyncClient.getBlobAsyncClient(path)).thenReturn(blobAsyncClient);
-        when(filePart.content()).thenReturn(Flux.just(new DefaultDataBufferFactory().wrap(new byte[]{1,2,3,4})));
+        when(filePart.content()).thenReturn(Flux.just(new DefaultDataBufferFactory().wrap(new byte[]{1, 2, 3, 4})));
         when(blobAsyncClient.upload(any(), any(), eq(true))).thenReturn(Mono.just(new BlockBlobItem(null, null, null, false, null)));
 
         // when
@@ -58,11 +57,33 @@ class AzureStorageServiceTest {
 
         // then
         StepVerifier.create(result)
-                .expectSubscription()
-                .assertNext(pathFromMono -> assertEquals(resultPath, pathFromMono))
-                .verifyComplete();
+                    .expectSubscription()
+                    .assertNext(pathFromMono -> assertEquals(resultPath, pathFromMono))
+                    .verifyComplete();
         verify(blobContainerAsyncClient).getBlobAsyncClient(path);
         verify(filePart).content();
         verify(blobAsyncClient).upload(any(), any(), eq(true));
     }
+
+    @Test
+    void deleteFile() {
+        // given
+        String pathFromDb = "https://example.blob.core.windows.net/example/app/path";
+        String pathInContainer = "app/path";
+        var document= DocumentEntity.builder().path(pathFromDb).build();
+        when(blobContainerAsyncClient.getBlobAsyncClient(pathInContainer)).thenReturn(blobAsyncClient);
+        when(blobAsyncClient.delete()).thenReturn(Mono.empty());
+
+        // when
+        Mono<Boolean> result = azureStorageService.deleteFile(document);
+
+        // then
+        StepVerifier.create(result)
+                    .expectSubscription()
+                    .assertNext(Assertions::assertTrue)
+                    .verifyComplete();
+        verify(blobContainerAsyncClient).getBlobAsyncClient(pathInContainer);
+        verify(blobAsyncClient).delete();
+    }
+
 }
